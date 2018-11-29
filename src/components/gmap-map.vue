2148092629f778444725b55c2d1c1b25e1c736e6
sggv950 <38014323+sggv950@users.noreply.github.com>
2018-11-29 18:19:30 +0200

    on fixing event details

---
+++ src/components/gmap-map.vue
@@ -0,0 +1,60 @@
+<template>
+  <GmapMap
+    v-if="center"
+    ref="mapRef"
+    class="static map"
+    :zoom="18"
+    :center="center"
+    map-type-id="roadmap"
+    style="height: 300px"
+  >
+    <GmapMarker
+      v-if="markers.position"
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      @click="center=m.position"
+    />
+  </GmapMap>
+</template>
+
+<script>
+const axios = require('axios');
+export default {
+  props: ["currEvent"],
+  data() {
+    return {
+      markers: [
+        {
+          position: null
+        }
+      ],
+      center: null
+    }
+  },
+  methods: {
+      getCoorFromAddress(location){
+      var currEventLocStr = `${location.address.replace(/[^a-zA-Z0-9]/g,'+')}+${location.city.replace(/[^a-zA-Z0-9]/g,'+')}`
+      return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${currEventLocStr}&key=AIzaSyC1FhnnrcBKyOeZF9as6Qw89mBzjul9jU4`)
+      .then(res => {
+        console.log(this.$refs.mapRef)
+        var latlng = res.data.results[0].geometry.location
+        this.center = latlng
+        this.markers.position.push(latlng)
+        return latlng
+        }).then(latlng => {
+          this.$refs.mapRef.panTo(latlng)
+        })
+      // console.log(currEventLocStr);
+    }
+  },
+  created(){
+      this.getCoorFromAddress(this.currEvent.location)
+  }
+  }
+
+</script>
+
+<style>
+</style>
