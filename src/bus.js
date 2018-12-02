8b3b0f88d64a320d5b0854a0575fb1a2a6f14356
Noam Kleiner <nmkleiner@gmail.com>
2018-12-02 09:25:31 +0200

    msg

---
+++ src/bus.js
@@ -0,0 +1,8 @@
+export const MSG = "show-user-msg";
+import Vue from "vue";
+
+// export const EMAIL_WAS_READ = 'x';
+// export const USER_LOGGEDIN = 'y';
+
+var bus = new Vue();
+export default bus;
---
+++ src/components/msg-msg.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="msg-msg" :class="{show: isShowing}">
+        <h3>{{msg}}</h3>
+    </section>
+</template>
+
+<script>
+import bus, { MSG } from "../bus.js";
+
+export default {
+    data() {
+        return {
+            isShowing: false,
+            msg: 'event updated'
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    created() {
+        bus.$on(MSG, (msg) => {
+            this.msg = msg
+            this.isShowing = !this.isShowing
+            setTimeout(() => {
+                this.isShowing = !this.isShowing
+            }, 2500);
+        })
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    
+</style>
